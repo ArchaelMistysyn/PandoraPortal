fde233b7e207ff37431360ee3ba6c3507b6ba664
Archael <Archael@hotmail.ca>
2024-12-11 11:09:53 -0500

    2.6pre - setup for gallery enhancements

--- gallery.php
+++ gallery.php
@@ -10,34 +10,33 @@
 </head>
 <body>
 	<header id="header"></header>
-	<main id="gallery-main">
+  <main id="gallery-main">
     <div id="lightbox" class="lightbox hidden">
       <span id="lightbox-close" class="lightbox-close">&times;</span>
-      <div class="lightbox-main-content">
-        <img id="lightbox-image" src="" alt="Lightbox Image" />
-        <div class="carousel-container">
-          <button id="carousel-prev" class="carousel-control"><img src="/images/Icons/arrow-square-left-fill.png" alt="carousel left"></button>
-          <div id="carousel" class="carousel">
-          </div>
-          <button id="carousel-next" class="carousel-control"><img src="/images/Icons/arrow-square-right-fill.png" alt="carousel left"></button>
-        </div>
-      </div>
+      <div class="lightbox-main-content"><img id="lightbox-image" src="" alt="Lightbox Image" /></div>
     </div>
-		<div id="gallery-main-container">
-			<div id="tab-menu">
-        <input type="text" id="filter-input" placeholder="Filter Content" oninput="filterItems()">
-        <div class="tab-menu" id="tabMenu"></div>
-      </div>
-			<div id="gallery-content-container">
-				<div id="tab-content">
-          <div id="lightbox" class="lightbox hidden">
-            <span id="lightbox-close" class="lightbox-close">&times;</span>
-            <img id="lightbox-image" src="" alt="Lightbox Image" />
+    <div id="gallery-main-container">
+      <div id="gallery-top-container">
+        <div id="tab-menu">
+          <input type="text" id="filter-input" placeholder="Filter Content" oninput="filterItems()">
+          <div class="tab-menu" id="tabMenu"></div>
+        </div>
+        <div id="display-container">
+          <div id="display-content">
           </div>
         </div>
-			</div>
-		</div>
-	</main>
+      </div>
+      <div id="gallery-bottom-container"><div id="carousel-container">
+        <button id="carousel-prev" class="carousel-control"><img src="/images/Icons/arrow-square-left-fill.png" alt="carousel left"></button>
+        <div id="tab-content" class="carousel"></div>
+        <button id="carousel-next" class="carousel-control"><img src="/images/Icons/arrow-square-right-fill.png" alt="carousel right"></button>
+      </div></div>
+    </div>
+    <div id="lightbox" class="lightbox hidden">
+      <span id="lightbox-close" class="lightbox-close">&times;</span>
+      <img id="lightbox-image" src="" alt="Lightbox Image" />
+    </div>
+  </main>
 	<script>
         const subcontentFolder = "galleryPages";
         const segments = {
