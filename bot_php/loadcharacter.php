b5b07660f20f7ae1445ba3085249bfd297245f3d
Archael <Archael@hotmail.ca>
2025-01-09 16:38:40 -0500

    5.13 attempt 3 char transfer

--- bot_php/loadcharacter.php
+++ bot_php/loadcharacter.php
@@ -1,8 +1,17 @@
 <?php
-session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['search_input']) && ctype_digit($_GET['search_input'])) {
-    $_SESSION['post_data'] = ['search_input' => $_GET['search_input']];
-    header('Location: ../characters.php');
+    $postData = ['search_input' => $_GET['search_input']];
+    $ch = curl_init('https://pandoraportal.ca/characters.php');
+    curl_setopt_array($ch, [
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $postData,
+        CURLOPT_RETURNTRANSFER => true,
+    ]);
+    $response = curl_exec($ch);
+    $httpStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    http_response_code($httpStatus);
+    echo $response;
     exit;
 }
 http_response_code(400);
