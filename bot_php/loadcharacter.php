c392bf2714c728c27022bd20ef552cae1050411c
Archael <Archael@hotmail.ca>
2025-01-09 16:36:00 -0500

    5.12 update char transfer handling attempt 2

--- bot_php/loadcharacter.php
+++ bot_php/loadcharacter.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['search_input']) && ctype_digit($_GET['search_input'])) {
     $_SESSION['post_data'] = ['search_input' => $_GET['search_input']];
-    header('Location: characters.php');
+    header('Location: ../characters.php');
     exit;
 }
 http_response_code(400);
