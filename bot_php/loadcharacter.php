143aaba93c8a42392c104cc4c4525d276dfe8da9
Archael <Archael@hotmail.ca>
2025-01-09 16:34:57 -0500

    5.11 update transfer code

--- bot_php/loadcharacter.php
+++ bot_php/loadcharacter.php
@@ -1,15 +1,11 @@
 <?php
+session_start();
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['search_input']) && ctype_digit($_GET['search_input'])) {
-    $ch = curl_init('https://pandoraportal.ca/characters.php');
-    curl_setopt_array($ch, [
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => ['search_input' => $_GET['search_input']],
-        CURLOPT_RETURNTRANSFER => true
-    ]);
-    echo $response = curl_exec($ch);
-    http_response_code(curl_getinfo($ch, CURLINFO_HTTP_CODE));
-    curl_close($ch);
-} else {
-    http_response_code(400);
-    exit('Invalid or missing input.');
+    $_SESSION['post_data'] = ['search_input' => $_GET['search_input']];
+    header('Location: characters.php');
+    exit;
 }
+http_response_code(400);
+exit('Invalid or missing input.');
+?>
