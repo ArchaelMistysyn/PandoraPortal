fc168e7f775df1599993342e3db63411e613203c
Archael <Archael@hotmail.ca>
2025-01-26 16:17:24 -0500

    7.1 Cleaned up initial UI page

---
+++ scripts/play_buttons.js
@@ -0,0 +1,31 @@
+const loadscreen = document.getElementById("loadscreen");
+const selectMenu = document.getElementById("interface-screen");
+    
+
+function clearScreens() {
+    loadscreen.style.display = 'None';
+}
+
+function onTravel() {
+    clearScreens();
+}
+
+function onQuest() {
+    clearScreens();
+}
+
+function onBattle() {
+    clearScreens();
+}
+
+function onGear() {
+    clearScreens();
+}
+
+function onInventory() {
+    clearScreens();
+}
+
+function onLore() {
+    clearScreens();
+}
