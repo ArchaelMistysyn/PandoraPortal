4a3f8156596684d05381c064490d12f2b12dd11a
Archael <Archael@hotmail.ca>
2025-02-02 18:44:52 -0500

    7.8 secured .env file

--- bot_php/db_queries.php
+++ bot_php/db_queries.php
@@ -1,6 +1,8 @@
 <?php
-$envFile = "/home/kylep910/pandoraportal.ca/nonpublic/.env";
+$webEnvFile = "/home/kylep910/PandoraPortalEnv/.env";
+$localEnvFile = "./nonpublic/.env";
 
+$envFile = file_exists($localEnvFile) ? $localEnvFile : $webEnvFile;
 if (file_exists($envFile)) {
     $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
@@ -8,13 +10,14 @@ if (file_exists($envFile)) {
         putenv($line);
     }
 }
-$DB_SERVER = getenv('DB_SERVER');
+$DB_SERVER   = getenv($envFile === $localEnvFile ? 'DB_SERVER_REMOTE' : 'DB_SERVER_LIVE');
 $DB_USERNAME = getenv('DB_USERNAME');
 $DB_PASSWORD = getenv('DB_PASSWORD');
 $DB_NAME = getenv('DB_NAME');
 
 function run_query($query, $return_value = true) {
-    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    global $DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME;
+    $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
