b293128603d96489337b8fcd7fa700f76890bdc7
Archael <Archael@hotmail.ca>
2025-02-25 18:28:22 -0500

    8.6 Lore Menu done. no content

--- scripts/play_buttons.js
+++ scripts/play_buttons.js
@@ -9,6 +9,9 @@ const forgeContainer = document.getElementById("forge-container");
 const forgeItemScreen = document.getElementById("forge-item-screen");
 const forgeMenu = document.getElementById('forge-menu');
 const blockingScreen = document.getElementById("blocking-screen");
+const loreContainer = document.getElementById("lore-container");
+const loreScreen = document.getElementById("lore-screen");
+const loreMenu = document.getElementById("lore-menu");
 const slotTypes = {"W": "Weapon", "A": "Armour", "V": "Greaves", "Y": "Amulet", "R": "Ring", "G": "Wings", "C": "Crest"};
 
 document.getElementById("lightbox-screen").addEventListener("click", function(event) {
@@ -35,10 +38,5 @@ function onQuest() {
 }
 
 function onBattle() {
-    clearScreens();
-}
-
-
-function onLore() {
     clearScreens();
 }
