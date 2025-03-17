7ef668e6c9cd7ba150f8466a3c3f85badab2a64c
Archael <Archael@hotmail.ca>
2025-03-17 10:34:39 -0400

    8.8 add 2 paragon

--- scripts/play_buttons.js
+++ scripts/play_buttons.js
@@ -15,6 +15,7 @@ const loreMenu = document.getElementById("lore-menu");
 const battleContainer = document.getElementById("battle-container");
 const battleScreen = document.getElementById("battle-screen");
 const battleMenu = document.getElementById("battle-menu");
+const magnitudeSlider = document.getElementById("magnitude-slider");
 const slotTypes = {"W": "Weapon", "A": "Armour", "V": "Greaves", "Y": "Amulet", "R": "Ring", "G": "Wings", "C": "Crest"};
 
 document.getElementById("lightbox-screen").addEventListener("click", function(event) {
